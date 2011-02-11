ebc0c69f20f0169b59cb0cdbdceaa4bd876e0adc
Brian Knep <bkpub@blep.com>
2011-02-11 19:37:17 +0000

    changed to port 8000

--- jumbotron/params.js
+++ jumbotron/params.js
@@ -7,7 +7,7 @@ module.exports = {
 
     // Server listens on this port. NOTE: If you change this, also
     // change it in the apache configs and in jjclient.js.
-    port: 8077,
+    port: 8000,
 
     // Regexp for allowable jumbotron names
     jumbotronRegExp:  /^[a-zA-Z0-9][a-zA-Z0-9_-]*$/,
