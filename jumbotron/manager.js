3d5eb54ad68f2dd661caaeaade0b01bc1ef7ccbf
Brian Knep <bleppie@dhcp-0147362475-e9-94.client.fas.harvard.edu>
2011-03-29 14:58:33 -0400

    move jumbotron management out of jjserver

---
+++ jumbotron/manager.js
@@ -0,0 +1,66 @@
+// ======================================================================
+// Manager
+
+var Image = require('./image');
+var Store = require('./store');
+var utils = require('./utils');
+
+// ----------------------------------------------------------------------
+// Manager
+
+function Manager(options) {
+    this._store = new Store(options);
+}
+
+// Subclass and Members
+Manager.prototype = utils.inherits(Store, { 
+    
+    // ----------------------------------------------------------------------
+    // Deal with storage
+
+    // Return the named jumbotron
+    getJumbotron: function getJumbotron(name, cb) {
+	this._store.getJumbotron(name, cb);
+    },
+
+    // Adds a jumbotron. "duplicate" error if a jumbotron
+    // with the same name exists.
+    addJumbotron: function addJumbotron(jumbotron, cb) {
+	this._store.addJumbotron(jumbotron, cb);
+    },
+	    
+    removeJumbotron: function removeJumbotron(jumbotron, cb) {
+	this._store.removeJumbotron(jumbotron, cb);
+    },
+
+    commitJumbotron: function commitJumbotron(jumbotron, cb) {
+	this._store.commitJumbotron(jumbotron, cb);
+    },
+
+    // ----------------------------------------------------------------------
+    // Easy access to displays and controllers
+
+    // Return the display with the given clientId, if any
+    getDisplay: function getDisplay(jName, clientId, cb) {
+	this.getJumbotron(jName, function(err, jumbotron) {
+	    if (err)
+		return cb(err);
+	    if (! jumbotron)
+		return cb("no jumbotron");
+	    cb(null, jumbotron.getDisplay(clientId));
+	});
+    },
+
+    // Return the controller with the given clientId, if any
+    getController: function getController(jName, clientId, cb) {
+	this.getJumbotron(jName, function(err, jumbotron) {
+	    if (err)
+		return cb(err);
+	    if (! jumbotron)
+		return cb("no jumbotron");
+	    cb(null, jumbotron.getController(clientId));
+	});
+    }
+});
+
+module.exports = Manager;
